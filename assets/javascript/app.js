b5396cfaf31dab5efa55b73d978693c55e913be4
Devin Hargrove <dhargrove1@rollins.edu>
2018-04-02 21:57:40 -0400

    adding google api

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,23 @@
 /*
 logic for Dealwitit
 */
+// Google Maps API to display on the page
+var map;
+var infoWindow;
+var request;
+var service;
+var markers = [];
+    function initialize() {
+    var center = new google.maps.LatLng(28.443237, -81.469780);
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: center,
+        zoom: 13
+        });
+    
+    // Informing maps how far search (radius) and what to display
+    request = {
+        location: center,
+        radius: 8047,
+        types: ['car_rental']
+    };
+    infowindow = new google.maps.InfoWindow();
