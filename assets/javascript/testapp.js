cea7215ff76d9b2f05fcf5a17103563f22c544a0
Camilo Gonzalez <CA@Cashs-MacBook-Pro.local>
2018-04-03 23:12:14 -0400

    Fix API sorting restaurants from ascending to descending order and by rating.

--- assets/javascript/testapp.js
+++ assets/javascript/testapp.js
@@ -3,10 +3,32 @@
     
 
     $.ajax({
-        url: "https://developers.zomato.com/api/v2.1/search?ent   ity_id=601&entity_type=city&count=30&sort=rating&order=asc&apikey=7c7844a8b5c9183b9520d3d76a131149",
+        url: "https://developers.zomato.com/api/v2.1/search?entity_id=601&entity_type=city&count=30&sort=rating&order=desc&apikey=7c7844a8b5c9183b9520d3d76a131149",
         method: "GET"
     })
     .then(function(response) {
         // Storing an array of results in the results variable
         console.log(response)
-        var results = response.data;})})
+        var results = response.data;
+         // Looping through each result item
+         for (var i = 0; i < results.length; i++) {
+
+            // storing a div tag
+            var restaurantDiv = $("#restaurants");
+
+            // Creating a paragraph tag with the results
+            var p = $("<p>").text(results[i]);
+
+            
+
+            // Appending the paragraph tag to the restaurantDiv
+            resturantDiv.append(p);
+            
+    
+    
+    
+    
+    
+         }
+        
+        })})
